96e5877ca70c625a894759bb6599b8c7e3940aac
Leonardo Matos <leomp120894@gmail.com>
2018-11-11 17:30:08 -0200

    handling callback request

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ const sqlite = require('sqlite3').verbose()
 
 // setup database and table
 const setup = dbFilename => {
-  return new Promise((resolve, reject) => {
+  return new Promise(resolve => {
+    const table = 'ecomplus_app_auth'
     const db = new sqlite.Database(dbFilename, () => {
-      db.run(`CREATE TABLE IF NOT EXISTS ecomplus_app_auth (
+      db.run('CREATE TABLE IF NOT EXISTS ' + table + ` (
         created_at                  DATETIME  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
         application_id              INTEGER   NOT NULL  PRIMARY KEY,
         application_app_id          INTEGER   NOT NULL,
@@ -19,16 +20,17 @@ const setup = dbFilename => {
         access_token                TEXT
       );`, ready)
     })
+    const client = { db, table }
 
     // resolve promise with lib methods when DB is ready
     const ready = () => {
       resolve({
-        getToken: require('./lib/methods/get-token.js'),
-        handleCallback: require('./lib/methods/handle-callback.js')
+        getAuth: require('./lib/methods/get-auth.js')(client),
+        handleCallback: require('./lib/methods/handle-callback.js')(client)
       })
     }
     // update access tokens periodically
-    require('./lib/services/update-tokens.js')
+    require('./lib/services/update-tokens.js')(client)
   })
 }
 
