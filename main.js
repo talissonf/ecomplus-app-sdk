93f7ed63a1220688360dbc07e5f46ef4cb3cdfea
Leonardo Matos <leomp120894@gmail.com>
2018-11-12 19:57:45 -0200

    handling setup errors, echo database info

--- main.js
+++ main.js
@@ -18,31 +18,42 @@ const axios = require('axios').create({
 
 // setup database and table
 const setup = dbFilename => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const table = 'ecomplus_app_auth'
-    const db = new sqlite.Database(dbFilename, () => {
-      db.run('CREATE TABLE IF NOT EXISTS ' + table + ` (
-        created_at                  DATETIME  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-        updated_at                  DATETIME  NOT NULL  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        application_id              INTEGER   NOT NULL  PRIMARY KEY,
-        application_app_id          INTEGER   NOT NULL,
-        application_title           VARCHAR   NOT NULL,
-        authentication_id           INTEGER   NOT NULL,
-        authentication_permissions  TEXT,
-        store_id                    INTEGER   NOT NULL,
-        access_token                TEXT
-      );`, ready)
+    const db = new sqlite.Database(dbFilename, err => {
+      if (err) {
+        reject(err)
+      } else {
+        db.run('CREATE TABLE IF NOT EXISTS ' + table + ` (
+          created_at                  DATETIME  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+          updated_at                  DATETIME  NOT NULL  DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+          application_id              INTEGER   NOT NULL  PRIMARY KEY,
+          application_app_id          INTEGER   NOT NULL,
+          application_title           VARCHAR   NOT NULL,
+          authentication_id           INTEGER   NOT NULL,
+          authentication_permissions  TEXT,
+          store_id                    INTEGER   NOT NULL,
+          access_token                TEXT
+        );`, ready)
+      }
     })
     const client = { db, table, axios }
 
     // resolve promise with lib methods when DB is ready
-    const ready = () => {
-      resolve({
-        getAuth: require('./lib/methods/get-auth.js')(client),
-        handleCallback: require('./lib/methods/handle-callback.js')(client),
-        apiRequest: require('./lib/methods/api-request.js')(client),
-        refreshToken: require('./lib/methods/refresh-token.js')(client)
-      })
+    const ready = err => {
+      if (!err) {
+        resolve({
+          getAuth: require('./lib/methods/get-auth.js')(client),
+          handleCallback: require('./lib/methods/handle-callback.js')(client),
+          apiRequest: require('./lib/methods/api-request.js')(client),
+          refreshToken: require('./lib/methods/refresh-token.js')(client),
+          // echo database info
+          dbFilename,
+          table
+        })
+      } else {
+        reject(err)
+      }
     }
     // update access tokens periodically
     require('./lib/services/update-tokens.js')(client)
