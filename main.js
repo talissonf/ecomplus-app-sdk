c12e1fecb75ebd28f308b724782340da3543d75b
Leonardo Matos <leomp120894@gmail.com>
2018-11-27 12:29:56 -0200

    keep returned client and promise

--- main.js
+++ main.js
@@ -16,47 +16,58 @@ const axios = require('axios').create({
   axios.defaults.headers[method]['Content-Type'] = 'application/json'
 })
 
+// keep returned client and promise
+let promise, client
+
 // setup database and table
 const setup = dbFilename => {
-  return new Promise((resolve, reject) => {
-    const table = 'ecomplus_app_auth'
-    const db = new sqlite.Database(dbFilename, err => {
-      if (err) {
-        reject(err)
-      } else {
-        // try to run first query creating table
-        db.run('CREATE TABLE IF NOT EXISTS ' + table + ` (
-          created_at                  DATETIME  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-          updated_at                  DATETIME  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-          application_id              VARCHAR   NOT NULL,
-          application_app_id          INTEGER   NOT NULL,
-          application_title           VARCHAR   NOT NULL,
-          authentication_id           VARCHAR   NOT NULL  PRIMARY KEY,
-          authentication_permissions  TEXT,
-          store_id                    INTEGER   NOT NULL,
-          access_token                TEXT
-        );`, ready)
+  dbFilename = dbFilename || process.env.ECOM_AUTH_DB || process.cwd() + '/db.sqlite3'
+  if (!client || client.dbFilename !== dbFilename) {
+    // handle new promise
+    promise = new Promise((resolve, reject) => {
+      const table = 'ecomplus_app_auth'
+
+      // init SQLite3 client with database filename
+      // reject all on error
+      const db = new sqlite.Database(dbFilename, err => {
+        if (err) {
+          reject(err)
+        } else {
+          // try to run first query creating table
+          db.run('CREATE TABLE IF NOT EXISTS ' + table + ` (
+            created_at                  DATETIME  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+            updated_at                  DATETIME  NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+            application_id              VARCHAR   NOT NULL,
+            application_app_id          INTEGER   NOT NULL,
+            application_title           VARCHAR   NOT NULL,
+            authentication_id           VARCHAR   NOT NULL  PRIMARY KEY,
+            authentication_permissions  TEXT,
+            store_id                    INTEGER   NOT NULL,
+            access_token                TEXT
+          );`, ready)
+        }
+      })
+      client = { dbFilename, db, table, axios }
+
+      // resolve promise with lib methods when DB is ready
+      const ready = err => {
+        if (!err) {
+          resolve({
+            getAuth: require('./lib/methods/get-auth.js')(client),
+            handleCallback: require('./lib/methods/handle-callback.js')(client),
+            apiRequest: require('./lib/methods/api-request.js')(client),
+            refreshToken: require('./lib/methods/refresh-token.js')(client)
+          })
+        } else {
+          reject(err)
+        }
       }
+
+      // update access tokens periodically
+      require('./lib/services/update-tokens.js')(client)
     })
-    const client = { db, table, axios }
-
-    // resolve promise with lib methods when DB is ready
-    const ready = err => {
-      if (!err) {
-        resolve({
-          getAuth: require('./lib/methods/get-auth.js')(client),
-          handleCallback: require('./lib/methods/handle-callback.js')(client),
-          apiRequest: require('./lib/methods/api-request.js')(client),
-          refreshToken: require('./lib/methods/refresh-token.js')(client)
-        })
-      } else {
-        reject(err)
-      }
-    }
-
-    // update access tokens periodically
-    require('./lib/services/update-tokens.js')(client)
-  })
+  }
+  return promise
 }
 
 module.exports = setup
